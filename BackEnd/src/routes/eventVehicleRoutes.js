f461fc81c391cb0c7b3911703fbd8c21fb6d6737
Lily Barrett <11barrettpl@gmail.com>
2026-01-18 11:24:11 +1300

    Tested and finalised event vehicle table

--- BackEnd/src/routes/eventVehicleRoutes.js
+++ BackEnd/src/routes/eventVehicleRoutes.js
@@ -6,9 +6,11 @@ const router = express.Router();
 
 router.use(authMiddleware);
 
-router.post('/', EventVehicleController.createAssignment );
-router.get('/vehicle/:vehicleId', EventVehicleController.getVehicleForEvent);
-router.get('/event/:eventId', EventVehicleController.getEventForVehicle);
+router.post('/', EventVehicleController.createAssignment);
+// Return a list of events that this vehicle has/ is attending
+router.get('/event/:vehicleId', EventVehicleController.getEventForVehicle);
+//  Get a list of vehicles that are attending/ did attend an event
+router.get('/vehicle/:eventId', EventVehicleController.getVehicleForEvent);
 router.delete('/:id', EventVehicleController.archiveAssignment);
 router.patch('/:id/unarchive', EventVehicleController.unArchiveAssignment);
 
