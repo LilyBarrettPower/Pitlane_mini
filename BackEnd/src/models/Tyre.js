69f4cd7585c778118f35df1ff09027a73936da99
Lily Barrett <11barrettpl@gmail.com>
2026-01-22 10:32:38 +1300

    Create static tyre model

---
+++ BackEnd/src/models/Tyre.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const TyreSchema = new mongoose.Schema(
+    {
+        organisationId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Organisation',
+            required: true,
+        },
+        vehicleId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Vehicle',
+            required: true,
+        },
+        brand: { type: String, required: true, trim: true },
+        spec: { type: String, required: true, trim: true },
+        fiaSerial: { type: String, trim: true, unique: true, sparse: true},
+        condition: { type: String, enum: ["New", "Used"], required: true }, // Should make it so that when this is "New" km total initially is 0
+        heatCycles: { type: Number, default: 0, min: 0 },
+        kmTotal: { type: Number, default: 0, min: 0 },
+        isActive: {
+            type: Boolean,
+            default: true,
+        },
+    },
+    { timestamps: true }
+);
+
+TyreSchema.index(
+    { organisationId: 1, vehicleId: 1, fiaSerial: 1 },
+    { unique: true, sparse: true }
+)
+
+module.exports = mongoose.model('Tyre', TyreSchema);
