c5a71b9ed07d997f64461fdc76743a6627c23afc
Lily Power <powerperformance@gmail.com>
2026-03-29 10:30:24 +1300

    create set up model

---
+++ BackEnd/src/models/SetUp.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+const SetUpSchema = new mongoose.Schema({
+  organisationId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Organisation',
+    required: true,
+  },
+  vehicleId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Vehicle',
+    required: true,
+  },
+  version: {type: String},
+  springNm: {
+    front: Number,
+    rear: Number
+  },
+  arbPos: {
+    front: Number,
+    rear: Number
+  },
+  rideHeight: {
+    front: Number, 
+    rear: Number
+  },
+  camber: {
+    front: String,
+    rear: String
+  },
+  toe: {
+    front: String,
+    rear: String
+  },
+  packers:{
+    front: String, 
+    rear: String
+  },
+  diffPreload: {type: Number},
+  brakeBias: {type: String},
+  wingHole: {type: String},
+  splitter: {type: String},
+  notes: { type: String, default: '' },
+  isActive: { type: Boolean, default: true },
+}, { timestamps: true });
+
+SetUpSchema.index({ organisationId: 1, vehicleId: 1 });
+
+
+module.exports = mongoose.model('SetUp', SetUpSchema);
